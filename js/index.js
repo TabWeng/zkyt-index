94d800b087db83d08cd90492ab22be1aebad1993
TabWeng <hlwyfeng@gmail.com>
2016-06-14 16:55:46 +0800

    中科研拓_前端实习笔试题
    中科研拓_前端实习笔试题，独立完成官网首页设计

---
+++ js/index.js
@@ -0,0 +1,56 @@
+$(function(){
+
+/**************
+*函数调用
+**************/
+setHeadPic();
+setHeadWord();
+turnCooperation();
+
+$(window).resize(function() {
+	setHeadPic();
+	setHeadWord();	
+});
+
+/*************
+*函数名：setHeadPic
+*功能：设置首页滚动窗的图片高度与浏览器等高
+*************/
+function setHeadPic(){
+	var getWindowHeight = $(window).height();
+	$(".img-indexHead").css("height", getWindowHeight + "px");
+}
+
+function setHeadWord(){
+
+	var getWindowHeight = $(window).height();
+	var getWindowWidth = $(window).width();
+
+	var getFirstPageWord = $("#p-indexHead");
+	getFirstPageWord.css("font-size", getWindowWidth/15.5 + "px");
+	getFirstPageWord.css("top", (getWindowHeight/2 - getWindowWidth/11)+ "px");
+
+	var getFirstPageWord2 = $("#p-indexHead-2 p");
+	getFirstPageWord2.css("font-size", getWindowWidth/15.5 + "px");
+
+	$("#p-indexHead-2").css("font-size", getWindowWidth/15.5 + "px");
+	$("#p-indexHead-2").css("top", (getWindowHeight/2 - getWindowWidth/5)+ "px");
+
+	$("#p-indexHead-2 div:last-child p:first-child").css("top", "-" + getWindowWidth/15.5*3 + "px");
+	$("#p-indexHead-2 div:last-child p:last-child").css("top", "-" + getWindowWidth/15.5*2 + "px");
+
+	$("#span-down span").css("top", getWindowHeight * 0.85);
+
+}
+
+function turnCooperation(){
+	$("#turnRight").click(function(){
+		$("#cooperationImage").animate({"background-position": "-=220px"});
+	});
+	$("#turnLeft").click(function(){
+		$("#cooperationImage").animate({"background-position": "+=220px"});
+	});	
+}
+
+});
+
